a023e55b5155b7f17e69b022ce623993a6aeee9c
Shohjahon <65382951+CrazyBoy621@users.noreply.github.com>
2023-01-19 00:00:58 +0500

    updated with details

--- medianTwoSortedArrays.js
+++ medianTwoSortedArrays.js
@@ -2,10 +2,13 @@
 // --------------------------------------------
 
 var findMedianSortedArrays = function (nums1, nums2) {
+  //Merge 2 arrays
   const fullArr = nums1.concat(nums2).sort(function (a, b) {
     return a - b;
   });
+  // Find middle index
   const middle = Math.trunc(fullArr.length / 2);
+
   if (fullArr.length % 2 === 0) {
     return (fullArr[middle] + fullArr[middle - 1]) / 2;
   } else {
@@ -13,6 +16,7 @@ var findMedianSortedArrays = function (nums1, nums2) {
   }
 };
 
+// Examples
 console.log(findMedianSortedArrays([1, 3], [2]));
 console.log(findMedianSortedArrays([1, 2], [3, 4]));
 console.log(findMedianSortedArrays([3], [-2, -1]));
